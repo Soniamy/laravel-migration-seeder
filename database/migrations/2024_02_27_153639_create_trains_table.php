59d44ca8b9be789ac35684530cb8be9c2ed3b77c
Soniamy <marianna.marzullo@gmail.com>
2024-02-27 17:34:51 +0100

    inserimento delle colonne della table più comando migration per aggiungerlo nel PHPmyadmin

--- database/migrations/2024_02_27_153639_create_trains_table.php
+++ database/migrations/2024_02_27_153639_create_trains_table.php
@@ -6,20 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code');
+            $table->integer('carriage_count');
+            $table->boolean('on_time')->default(true);
+            $table->boolean('canceled')->default(false);
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('trains');
